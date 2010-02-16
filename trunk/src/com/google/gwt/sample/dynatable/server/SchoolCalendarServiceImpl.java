f811b89fcaa09ef8aa901fe56a920c412648acba
Morswin <Morswin@f71a9e3a-1b4b-11df-8c64-7d73050a11b0>
2010-02-16 23:21:23 +0000

    Changed project structure to be a bit less random :P

--- trunk/src/com/google/gwt/sample/dynatable/server/SchoolCalendarServiceImpl.java
+++ trunk/src/com/google/gwt/sample/dynatable/server/SchoolCalendarServiceImpl.java
@@ -13,10 +13,10 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package com.google.gwt.sample.dynatable.server;
+package dynatable.server;
 
-import com.google.gwt.sample.dynatable.client.SchoolCalendarService;
-import com.google.gwt.sample.dynatable.utils.GWTService;
+import dynatable.client.SchoolCalendarService;
+import dynatable.utils.GWTService;
 import com.mits.core.client.Person;
 import service.UserManager;
 
--- trunk/src/com/google/gwt/sample/dynatable/utils/RPC_2_0_1.java
+++ trunk/src/com/google/gwt/sample/dynatable/utils/RPC_2_0_1.java
@@ -1,4 +1,4 @@
-package com.google.gwt.sample.dynatable.utils;
+package dynatable.utils;
 
 import com.google.gwt.user.client.rpc.IncompatibleRemoteServiceException;
 import com.google.gwt.user.client.rpc.RemoteService;
@@ -7,45 +7,11 @@ import com.google.gwt.user.client.rpc.impl.AbstractSerializationStream;
 import com.google.gwt.user.server.rpc.*;
 import com.google.gwt.user.server.rpc.impl.LegacySerializationPolicy;
 import com.google.gwt.user.server.rpc.impl.ServerSerializationStreamReader;
-import com.google.gwt.user.server.rpc.impl.ServerSerializationStreamWriter;
 import com.google.gwt.user.server.rpc.impl.TypeNameObfuscator;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.*;
-
-/*
- * Copyright 2007 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
-
-import com.google.gwt.user.client.rpc.IncompatibleRemoteServiceException;
-import com.google.gwt.user.client.rpc.RemoteService;
-import com.google.gwt.user.client.rpc.SerializationException;
-import com.google.gwt.user.client.rpc.impl.AbstractSerializationStream;
-import com.google.gwt.user.server.rpc.impl.LegacySerializationPolicy;
-import com.google.gwt.user.server.rpc.impl.ServerSerializationStreamReader;
-import com.google.gwt.user.server.rpc.impl.ServerSerializationStreamWriter;
-import com.google.gwt.user.server.rpc.impl.TypeNameObfuscator;
-
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
 
 /**
  * Utility class for integrating with the RPC system. This class exposes methods
